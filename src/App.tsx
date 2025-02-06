02581de0de98754bbb5a8f8c09214999b69a2e42
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-02-06 23:29:32 +0530

    Changes: Project Initiated & Setup Completed With ShadCn UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default App
